3bda2c214b7530a670a94788ba8a13386e73bf2c
Gontran Bezerra <gontranbezerra@gmail.com>
2021-03-21 09:17:47 -0300

    feat: completion of the exercise 'Desafio 03'

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,46 +23,127 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const selectedProduct = cart.find((product) => product.id === productId);
+      const stock: Stock = await api.get(`stock/${productId}`).then((response) => response.data);
+
+      if (stock.amount < 1) {
+        toast.error('Produto sem estoque');
+        return;
+      }
+
+      if (selectedProduct) {
+        const total = selectedProduct.amount + 1;
+
+        if (total > stock.amount) {
+          toast.error('Quantidade solicitada fora de estoque');
+          return;
+        }
+
+        const newCart = cart.map((item) => {
+          if (item.id === productId) {
+            return { ...item, amount: total };
+          } else {
+            return item;
+          }
+        });
+
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
+
+        setCart(newCart);
+
+        return;
+      }
+
+      const product: Product = await api
+        .get(`products/${productId}`)
+        .then((response) => response.data);
+
+      setCart((cart) => {
+        const newCart = [...cart, { ...product, amount: 1 }];
+
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
+
+        return newCart;
+      });
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const selectedProduct = cart.find((product) => product.id === productId);
+
+      if (!selectedProduct) {
+        throw new Error();
+      }
+
+      setCart((cart) => {
+        const newCart = cart.filter((product) => product.id !== productId);
+
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
+
+        return newCart;
+      });
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
-  const updateProductAmount = async ({
-    productId,
-    amount,
-  }: UpdateProductAmount) => {
+  const updateProductAmount = async ({ productId, amount }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return;
+      }
+
+      const selectedProduct = cart.find((product) => product.id === productId);
+
+      if (!selectedProduct) {
+        throw new Error();
+      }
+
+      const stock: Stock = await api.get(`stock/${productId}`).then((response) => response.data);
+
+      if (stock.amount < 1) {
+        toast.error('Produto sem estoque');
+        return;
+      }
+
+      if (amount > stock.amount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      setCart((cart) => {
+        const newCart = cart.map((product) => {
+          if (product.id === productId) {
+            product.amount = amount;
+          }
+          return product;
+        });
+
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
+
+        return newCart;
+      });
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
   return (
-    <CartContext.Provider
-      value={{ cart, addProduct, removeProduct, updateProductAmount }}
-    >
+    <CartContext.Provider value={{ cart, addProduct, removeProduct, updateProductAmount }}>
       {children}
     </CartContext.Provider>
   );
